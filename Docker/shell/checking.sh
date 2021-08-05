ea2f60cece717ed3888f8d33cececd613c834c92
EfimSirotkin <44836472+EfimSirotkin@users.noreply.github.com>
2021-08-05 09:03:54 +0300

    Update checking.sh

--- Docker/shell/checking.sh
+++ Docker/shell/checking.sh
@@ -1,4 +1,4 @@
-ID=$(docker container ls --format="{{.ID}}\t{{.Ports}}" | grep 80/tcp)
+ID=$(docker container ls --format="{{.ID}}\t{{.Ports}}" | grep 80/tcp | awk '{print $1}')
 if [ -n"$ID" ]; then
   docker container stop ${ID}
   docker container rm ${ID}
